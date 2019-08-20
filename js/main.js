4f80265734e4f2407f800f4ab0578832060cc79b
adam-weiler <adam.weiler@yahoo.ca>
2019-08-20 14:47:42 -0400

    Adding error class to username and password fields when user hits submit button.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // const status = document.querySelector('.status');
-
     const modal = document.querySelector('div.modal');
-
-    
-
+    // console.log(modal)
 
     const btnSignin = document.querySelector('button.signin');
     // console.log(btnSignin);
@@ -17,20 +13,28 @@ document.addEventListener('DOMContentLoaded', function() {
         modal.style.display = 'block';
         // console.log(modal.style.display);
 
-
-        // console.log(btnClose);
         const btnClose = document.querySelector('button.close');
+        // console.log(btnClose);
 
+        const btnSubmit = document.querySelector('button.submit');
+        // console.log(btnSubmit);
+
+        btnSubmit.addEventListener('click', (e) => {
+            const inputUser = document.querySelector('input#user');
+            // console.log(inputUser)
+
+            const inputPass = document.querySelector('input#pass');
+            // console.log(inputPass)
+            // modal.style.display = '';
+            // // console.log(modal.style.display);
+
+            inputUser.className = 'error'
+            inputPass.className = 'error'
+        });
 
         btnClose.addEventListener('click', (e) => {
             modal.style.display = '';
             // console.log(modal.style.display);
-    
-    
-            // console.log(btnClose);
-            
-    
-    
         });
 
 
